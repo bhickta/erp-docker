f0d1f04a130266b22654e0f6eaa0f8cd43cc9f3a
Your Name <you@example.com>
2024-10-22 08:24:49 +0530

    init pjj

--- development/installer.py
+++ development/installer.py
@@ -56,8 +56,8 @@ def get_args_parser():
         "--site-name",
         action="store",
         type=str,
-        help="Site name, should end with .localhost, default: development.localhost",  # noqa: E501
-        default="development.localhost",
+        help="Site name, should end with .localhost, default: pjj.localhost",  # noqa: E501
+        default="pjj.localhost",
     )
     parser.add_argument(
         "-r",
